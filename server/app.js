640fb3ae2c4f2a48abf8fcd651fd87c8f5fb25f9
Alexandre Zagame <zagamealexandre@gmail.com>
2021-01-15 12:24:29 +0100

    fixing errors

--- server/app.js
+++ server/app.js
@@ -3,9 +3,16 @@ const app = express();
 const request = require("request");
 const PORT = process.env.PORT || 8080;
 
+// app.use(express.static(path.join(__dirname, 'build')));
+
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 app.get("/", (req, res) => {
   res.send("YOO");
 });
+
 
 app.get("/getCoins", (req, res) => {
   request(
